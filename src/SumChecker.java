82572a88a985bb44cad4cb4ff6e632a962b5dcc2
lloydkev000 <lloydkevin631@gmail.com>
2024-09-18 13:35:25 -0400

    Finished!

---
+++ src/SumChecker.java
@@ -0,0 +1,16 @@
+public class SumChecker {
+    public static void main(String[] args) {
+        hasEqualSum(1, 1, 1);
+        hasEqualSum(1, 1, 1);
+        hasEqualSum(1, -1, 0 );
+    }
+    public static boolean hasEqualSum(int num1, int num2, int num3 ){
+        //find sum of num1 and num2 store it as a variable
+        //return whether that sum is equal to num 3
+        if(kilometersPerHour < 0){
+            return -1;
+        }
+    }
+}
+
+
